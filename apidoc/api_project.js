b33fb54f705c27de8cc36c3f658f0164b4ab191c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-16 01:38:29 +0000

    Deploy semojs/semo-starter-backend-typescript to github.com/semojs/semo-starter-backend-typescript.git:gh-pages

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-07-16T01:35:54.211Z",
+    "time": "2020-07-16T01:37:45.716Z",
     "url": "http://apidocjs.com",
     "version": "0.23.0"
   }
